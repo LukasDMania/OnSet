a18561085a500f5fdfc4c46d1b6cdfa72b735ef2
Lukas <lukasdestuyver2@gmail.com>
2026-03-10 23:00:56 +0100

    Small bug fix on the join project page

--- Pages/Projects/Index.cshtml.cs
+++ Pages/Projects/Index.cshtml.cs
@@ -1,7 +1,10 @@
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using OnSet.Features.Projects.Index;
+using OnSet.Features.Projects.Join;
+using OnSet.Infrastructure.Results;
 
 namespace OnSet.Pages.Projects;
 
@@ -19,10 +22,44 @@ public class IndexModel : PageModel
 
     public Model Data { get; private set; } = new();
 
+    [BindProperty]
+    public string JoinCode { get; set; } = string.Empty;
+
     public async Task OnGetAsync()
     {
         var userId = _currentUserService.UserId;
         Data = await _mediator.Send(new Query { UserId = userId });
+    }
+
+    public async Task<IActionResult> OnPostJoinAsync(string joinCode)
+    {
+        var userId = _currentUserService.UserId;
+
+        if (string.IsNullOrWhiteSpace(userId))
+        {
+            // Should not happen due to [Authorize], but keep it defensive.
+            return RedirectToPage("/Users/Login");
+        }
+
+        var command = new Command
+        {
+            JoinCode = joinCode,
+            UserId = userId
+        };
+
+        Result result = await _mediator.Send(command);
+
+        if (!result.Success)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error);
+            }
+        }
+
+        // Always reload the list so newly joined projects show up.
+        Data = await _mediator.Send(new Query { UserId = userId });
+        return Page();
     }
 }
 
